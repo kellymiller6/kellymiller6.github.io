e8a6636648f3f3af82e09c903329e9d282586a7a
Kelly Miller <kellyannemiller@gmail.com>
2017-07-30 00:08:43 -0600

    click out of iframe

--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
 $('.resume').on('click', function(){
-  $(this).parents().children('iframe').toggleClass('clicked')
-})
+  $(this).parents().children('iframe').toggleClass('clicked');
+});
 
 $('.resume').on('click', function(){
-  $(this).parents().children('button').toggleClass('active')
-})
+  $(this).parents().children('button').toggleClass('active');
+});
+
+$('article, h1, div').on('click', function(){
+  const resFrame = $(this).parents().children().children().children('nav').children('iframe');
+  const resBtn = $(this).parents().children().children().children('nav').children('button');
+
+  if(resFrame.hasClass('clicked')){
+    resFrame.removeClass('clicked');
+  }
+
+  if(resBtn.hasClass('active')){
+    resBtn.removeClass('active');
+  }
+});
